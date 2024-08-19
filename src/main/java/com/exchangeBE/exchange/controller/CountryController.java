dab6c689527cdd2f46870ad1c9547035b3352fb1
MuuiGong <muuigong@kakao.com>
2024-08-19 12:47:43 +0900

    trip 부분 주석 처리

--- src/main/java/com/exchangeBE/exchange/controller/CountryController.java
+++ src/main/java/com/exchangeBE/exchange/controller/CountryController.java
@@ -1,31 +1,31 @@
-package com.exchangeBE.exchange.controller;
-
-import com.exchangeBE.exchange.entity.Trip.Country;
-import com.exchangeBE.exchange.service.trip.CountryService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Map;
-
-@RestController
-@RequestMapping("/countries")
-@RequiredArgsConstructor
-public class CountryController {
-
-    private CountryService countryService;
-
-    @GetMapping
-    public Map<String, List<Country>> getCountries() {
-        return countryService.getCountries();
-    }
-
-    @GetMapping("/{region}")
-    public List<Country> getCountriesByRegion(@PathVariable String region) {
-        return countryService.getCountries().getOrDefault(region, List.of());
-    }
-}
+//package com.exchangeBE.exchange.controller;
+//
+//import com.exchangeBE.exchange.entity.Trip.Country;
+//import com.exchangeBE.exchange.service.trip.CountryService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.List;
+//import java.util.Map;
+//
+//@RestController
+//@RequestMapping("/countries")
+//@RequiredArgsConstructor
+//public class CountryController {
+//
+//    private CountryService countryService;
+//
+//    @GetMapping
+//    public Map<String, List<Country>> getCountries() {
+//        return countryService.getCountries();
+//    }
+//
+//    @GetMapping("/{region}")
+//    public List<Country> getCountriesByRegion(@PathVariable String region) {
+//        return countryService.getCountries().getOrDefault(region, List.of());
+//    }
+//}
