848571acfddca0bb5bf4633f4fec577d36ab104b
MuuiGong <muuigong@kakao.com>
2024-08-22 10:38:08 +0900

    보고서 목록 조회 swagger 어노테이션 추가

--- src/main/java/com/exchangeBE/exchange/controller/ReportController.java
+++ src/main/java/com/exchangeBE/exchange/controller/ReportController.java
@@ -141,6 +141,9 @@ public class ReportController {
     }
 
     @PostMapping("/type")
+    @Operation(summary = "보고서 목록 조회", description = "특정 사용자의 보고서 유형에 따른 보고서 목록을 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "보고서 목록 조회 성공", content = @Content(schema = @Schema(hidden = true)))
+    @ApiResponse(responseCode = "404", description = "보고서 목록 조회 실패", content = @Content(schema = @Schema(hidden = true)))
     public ResponseEntity getReportByType(@RequestBody UserReportTypeRequestDto userReportTypeRequestDto) {
         List<UserReportTypeResponseDto> userReportTypeResponseDtoList = reportService.getReportByType(userReportTypeRequestDto);
 
